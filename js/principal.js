c51c8059dd36e62a26a59033973fec2520f74bc7
Renato Soares <renatof.m.soares@gmail.com>
2018-05-29 00:35:41 -0300

    separando js do html

---
+++ js/principal.js
@@ -0,0 +1,8 @@
+console.log("Fui carregado de um arquivo externo");
+console.log(document);
+
+var titulo = document.querySelector(".titulo");
+console.log(titulo);
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent = "Aparecida Nutricionista"; 
